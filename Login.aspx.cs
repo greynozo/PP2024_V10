3bdb01432b3a55d7bd8537032d21eac861c21469
greynozo <kioc04@hotmail.com>
2024-08-28 16:10:26 -0300

    Arreglo Login

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -31,17 +31,24 @@ namespace PP2024_V10
                     //builder.UserID = "sa";
                     //builder.Password = "13213";
 
-                    string script = "SELECT USUARIO FROM USUARIOS WHERE USUARIO = '" + txtUsuario.Text + "' AND" +
+                    string script = "SELECT USUARIO FROM LOGIN WHERE USUARIO = '" + txtUsuario.Text + "' AND" +
                         " PASS = '" + txtPassword.Text + "'";
 
                     connection.Open();
 
                     SqlCommand command = new SqlCommand(script, connection);
 
-                    //SqlDataReader reader = command.ExecuteReader();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string user = reader.GetString(0);
+                    }
+
+                    reader.Close();
 
                     int filas = command.ExecuteNonQuery();
-
+              
                     connection.Close();
 
                     if (filas < 0)
